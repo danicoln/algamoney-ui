063c3edf5b38aca53158a8b4ebadebcc6bf42ca2
Daniel Lincoln <lincolndeveloper@outlook.com>
2023-12-21 16:47:16 -0300

    14.6. Criando um Feature Module

--- src/app/core/lancamentos/lancamentos.module.ts
+++ src/app/core/lancamentos/lancamentos.module.ts
@@ -1,12 +1,54 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DropdownModule } from 'primeng/dropdown';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { TooltipModule } from 'primeng/tooltip';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LancamentosPesquisaComponent,
+    LancamentoCadastroComponent,
+    LancamentosGridComponent //Usamos o grid apenas dentro de pesquisa, não sendo necessário então importá-lo
+
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule,
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    InputNumberModule,
+    InputMaskModule,
+    HttpClientModule
+  ],
+  exports: [
+    LancamentosPesquisaComponent,
+    LancamentoCadastroComponent,
+    LancamentosGridComponent
   ]
 })
 export class LancamentosModule { }
